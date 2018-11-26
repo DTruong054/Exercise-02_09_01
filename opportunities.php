0398af3eaabcb6585ba48cb6d06f58aaa9488583
Unknown <DTruong054@west-mec.org>
2018-11-26 14:26:39 -0800

    dio

--- opportunities.php
+++ opportunities.php
@@ -48,8 +48,8 @@
                 echo "<p>Unable to execute. Error code: " . mysqli_errno($DBConnect) . ": " . mysqli_error($DBConnect) . "</p>\n";
             } else {
                 if (mysqli_num_rows($queryResult) == 0) {
-                    ++$errors;
-                    // echo "<p>Invalid internID</p>\n";
+                    ++$errors; 
+                    echo "<p>Invalid internID</p>\n";
                 }
             }
         }
@@ -61,11 +61,56 @@
         }
         //DEBUG
         echo "internName $internName";
-
-        if ($DBConnect) {
-            echo "<p>Closing database <strong>$DBName</strong> connection</p>\n";
-            mysqli_close($DBConnect);
+        $tableName = "assigned_opportunities";
+        if ($errors == 0){
+            $SQLString = "SELECT COUNT(opportunitiesID) FROM $tableName HERE internID='$internID'" . 
+                        " AND dateApproved IS NOT NULL";
+            $queryResult = mysqli_query($DBConnect, $SQLString);
+            if (mysqli_num_rows($result) > 0) {
+                echo "<br>No syntax error\n";
+                $row  = mysqli_fetch_row($result);
+                $approvedOps = $row[0];
+                mysqli_free_result($queryResult);
+            }
+            $selectedOps = array();
+            $SQLString = "SELECT opportunityID FROM $tableName WHERE internID='$internID'";
+            $queryResult = mysqli_query($DBConnect, $SQLString);
+            if (mysqli_num_rows($result) > 0) {
+                while (($row = mysqli_fetch_row($queryResult)) != false) {
+                    $selectedOps[] = $row[0];
+                }
+                mysqli_free_result($queryResult);
+            }
         }
+        $assignedOps = array();
+        $SQLString = "SELECT COUNT(opportunitiesID) FROM $tableName HERE internID='$internID'" . 
+                    " AND dateApproved IS NOT NULL";
+                    $queryResult = mysqli_query($DBConnect, $SQLString);
+        if (mysqli_num_rows($result) > 0) {
+            while (($row = mysqli_fetch_row($queryResult)) != false) {
+                $assignedOps[] = $row[0];
+                    }
+                mysqli_free_result($queryResult);
+            }
+        $tableName = "opportunities";
+        $opportunities = array();
+        $SQLString = "SELECT oportunityID, company, city, startDate, endDate, position, description FROM $tableName";
+        //debug
+        echo "<br>" . mysqli_num_rows($queryResult) . "<br>\n";
+
+        if (mysqli_num_rows($result) > 0) {
+            while (($row = mysqli_fetch_assoc($queryResult)) != false) {
+                $opportunities[] = $row[0];
+            }
+            mysqli_free_result($queryResult);
+        }
+        //debug 
+        echo "<pre>";
+        print_r($opportunities);
+        echo "</pre>";
+        echo "<table border='1' width='100%'>\n";
+        echo "</table>";
+        echo "<p><a href='internLogin.php'>Log Out</a></p>"
     ?>
 </body>
 </html>
